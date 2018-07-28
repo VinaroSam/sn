47e95ab4e0bd77dc7ff3a7a6473e386b363c5fb4
VinaroSam <vinaro.sam@gmail.com>
2018-07-28 12:01:38 +0200

    change back to port 3000

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 3000,
   db: process.env.MONGODB_URI || 'mongodb://localhost:27017/sn',
   SECRET_TOKEN: 'lereseausocialdevinaro',
   SECRET_TOKEN_DECODE: 'lereseausocialdevinaro'
